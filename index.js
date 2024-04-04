2753ae0f242a3c46e09fa858cc6bd546b60f196d
MnLouis <luisguilhermesilvacosta58@gmail.com>
2024-04-03 21:57:28 -0300

    Pesistente

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 firebase.auth().onAuthStateChanged(user => {
     if (user) {
-        document.getElementById("a").innerHTML="Logado";
+        document.getElementById("a").innerHTML=user.email;
         var link = document.getElementById("a");
         link.getAttribute("href");
         link.setAttribute("href",
             "pages/User/user.html");
+        
     }else{
         document.getElementById("a").innerHTML="Iniciar Secção";
         var link = document.querySelector("a");
